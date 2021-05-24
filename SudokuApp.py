7c608dcf9b6083e7cc5bc6a8f5f4b88f72393814
ØNE <chonhungdieunhamnhi@gmail.com>
2021-05-24 12:57:49 -0400

    Add new button and display message

--- SudokuApp.py
+++ SudokuApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 
 
 boardTest = [
@@ -10,8 +10,29 @@ boardTest = [
     [0, 6, 0, 0, 8, 0, 3, 5, 0],
     [6, 2, 0, 0, 0, 3, 8, 0, 0],
     [0, 0, 9, 0, 0, 0, 0, 0, 0],
-    [8, 4, 0, 0, 7, 0, 9, 2, 0]
-]
+    [8, 4, 0, 0, 7, 0, 9, 2, 0]]
+
+newBoard = [
+    [1, 1, 1, 0, 1, 0, 0, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 3, 0, 0, 0, 4, 5],
+    [0, 7, 3, 0, 1, 0, 0, 9, 0],
+    [2, 1, 0, 1, 0, 4, 0, 7, 1],
+    [0, 6, 0, 0, 1, 0, 3, 5, 0],
+    [6, 2, 0, 0, 0, 3, 1, 0, 0],
+    [0, 0, 9, 0, 0, 0, 0, 0, 0],
+    [1, 4, 0, 0, 1, 0, 9, 1, 0]]
+
+result = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [9, 9, 9, 9, 9, 9, 9, 9, 9],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [8, 2, 8, 8, 8, 8, 8, 2, 8],
+    [8, 8, 3, 8, 8, 8, 3, 8, 8],
+    [8, 8, 8, 4, 9, 4, 8, 8, 8]]
 
 app = Flask(__name__)
 
@@ -21,16 +42,39 @@ def home():
     return render_template("board.html", board=boardTest)
 
 
-@app.route("/check_board", methods=['POST', 'GET'])
+@app.route("/check_board", methods=['POST'])
 def checkBoard():
     if request.method == 'POST':
         # This is where new board array send back from client side
         # Use 'board' to check if board is complete 
         board = request.get_json()
+        
+        message = ''
+        checkResult = True
+        if checkResult == True:
+            message = 'Well done'
+        elif checkResult == False:
+            message = 'Ehh... Try again please'
         # Test console
         for i in board:
             print(i)
-        return 'OKAY'
+        return message
+    else:
+        return render_template("board.html", board=boardTest)
+
+
+@app.route("/new_board", methods=['GET'])
+def getNewBoard():
+    if request.method == 'GET':
+        return jsonify(board = render_template("new_board.html", board=newBoard))
+    else:
+        return render_template("board.html", board=boardTest)
+
+
+@app.route("/result", methods=['GET'])
+def showResult():
+    if request.method == 'GET':
+        return jsonify(board = result)
     else:
         return render_template("board.html", board=boardTest)
 
--- static/JS/app.js
+++ static/JS/app.js
@@ -1,12 +1,23 @@
 function onlyNumberKey(evt) {
     // Only ASCII character in that range allowed
     var ASCIICode = evt.which ? evt.which : evt.keyCode;
-    if (ASCIICode > 31 && (ASCIICode < 49 || ASCIICode > 57)) 
-        return false;
+    if (ASCIICode > 31 && (ASCIICode < 49 || ASCIICode > 57)) return false;
     return true;
 }
 
-function getBoardArray() {
+function newBoard() {
+    $("div.message-container h3").css("display", "none");
+    $.ajax({
+        url: "/new_board",
+        type: "GET",
+        success: function (newBoard) {
+            $("tbody").html(newBoard.board);
+            console.log(newBoard.board);
+        },
+    });
+}
+
+function checkResult() {
     var boardArray = [];
     $("table#board tr").each(function () {
         var rowDataArray = [];
@@ -25,5 +36,31 @@ function getBoardArray() {
         type: "POST",
         data: JSON.stringify(boardArray),
         contentType: "application/json; charset=utf-8",
+        success: function(message) {
+            $("div.message-container h3").html(message);
+            $("div.message-container h3").css("display", "inline");
+        }
     });
+}
+
+function showResult() {
+    $.ajax({
+        url: "/result",
+        type: "GET",
+        success: function (result) {
+            boardArray = result.board;
+            row = 0;
+            $("table#board tr").each(function () {
+                col = 0;
+                $(this).find("input").each(function () {
+                    $(this).val(boardArray[row][col]);
+                    col += 1;
+                });
+                row += 1;
+            });
+        },
+    });
+    // Display message for when give up
+    $("div.message-container h3").html("Give up already?");
+    $("div.message-container h3").css("display", "inline");
 }
